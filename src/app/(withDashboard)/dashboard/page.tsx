efb40ee183f87e645089f0513a27ae61d458a762
Shanto93 <shantoislam7363@gmail.com>
2024-09-13 10:30:10 +0600

    Github Login is done

--- src/app/(withDashboard)/dashboard/page.tsx
+++ src/app/(withDashboard)/dashboard/page.tsx
@@ -1,8 +1,33 @@
-const DashboardPage = () => {
+import { authOptions } from "@/utils/authOptions";
+import { getServerSession, type Session } from "next-auth";
+import Image from "next/image";
+
+type DashboardPageProps = {
+  session: Session | null;
+};
+
+const DashboardPage = async () => {
+  const session = await getServerSession(authOptions);
+  console.log(session);
   return (
-    <div>
-      <h1 className="text-4xl text-center mt-10">Welcome To Dashboard Page</h1>
-    </div>
+    <>
+      {session?.user && (
+        <div>
+          <h1 className="text-4xl text-center mt-10">
+            Welcome {session?.user?.name}
+          </h1>
+          {session?.user?.image && (
+            <Image
+              src={session.user.image}
+              width={300}
+              height={300}
+              className="mx-auto rounded-full mt-5"
+              alt={`${session?.user?.name}'s Image`}
+            />
+          )}
+        </div>
+      )}
+    </>
   );
 };
 
--- src/utils/authOptions.ts
+++ src/utils/authOptions.ts
@@ -1,5 +1,12 @@
 import { type NextAuthOptions } from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
 
 export const authOptions: NextAuthOptions = {
-  providers: [],
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string
+    })
+  ],
+  secret: process.env.NEXT_SECRET
 };
